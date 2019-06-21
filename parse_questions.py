2b55bdacba8293238bee2b4dca5826c9f6561130
unknown <ykim164@illinois.edu>
2019-06-20 17:43:13 -0700

    Cleanup code and structure

--- parse_questions.py
+++ parse_questions.py
@@ -35,18 +35,13 @@ with open('data/leetcode_data_processed_synset2.csv', 'w', newline='', encoding=
         stat = json['stat']
         difficulty = json['difficulty']['level']
         question_name = stat['question__title_slug'] 
-        # print(question_name)
-
         # The pages does a POST request for dynamic content. Need to send a MySql query to database
         data = {"operationName":"questionData","variables":{"titleSlug":"{}".format(question_name)},"query":"query questionData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    boundTopicId\n    title\n    titleSlug\n    content\n    translatedTitle\n    translatedContent\n    isPaidOnly\n    difficulty\n    likes\n    dislikes\n    isLiked\n    similarQuestions\n    contributors {\n      username\n      profileUrl\n      avatarUrl\n      __typename\n    }\n    langToValidPlayground\n    topicTags {\n      name\n      slug\n      translatedName\n      __typename\n    }\n    companyTagStats\n    codeSnippets {\n      lang\n      langSlug\n      code\n      __typename\n    }\n    stats\n    hints\n    solution {\n      id\n      canSeeDetail\n      __typename\n    }\n    status\n    sampleTestCase\n    metaData\n    judgerAvailable\n    judgeType\n    mysqlSchemas\n    enableRunCode\n    enableTestMode\n    envInfo\n    libraryUrl\n    __typename\n  }\n}\n"}
-
         r = requests.post('https://leetcode.com/graphql', json = data).json()
-
         if r['data']['question']['content']:
             soup = bs(r['data']['question']['content'], 'html')
             title = r['data']['question']['title'] # question name
             topics = r['data']['question']['topicTags'] # get topic list
-            # print('Question = {}, topics = {}'.format(title, [item['name'] for item in topic_arr]))
             topic_arr = [item['name'].lower() for item in topics]
             content =  soup.get_text().replace('\n',' ') # question content
             tokens = content.split(' ')
