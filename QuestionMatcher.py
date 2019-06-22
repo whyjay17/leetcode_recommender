4c93244c7ec78dca909efa6ff4b697c28c91959d
unknown <ykim164@illinois.edu>
2019-06-22 00:47:01 -0700

    Implement QuestionMatcher class

--- QuestionMatcher.py
+++ QuestionMatcher.py
@@ -1,4 +1,5 @@
 import numpy as np
+# from nltk.corpus import stopwords
 
 class QuestionMatcher(object):
     """
@@ -11,9 +12,6 @@ class QuestionMatcher(object):
     
     Methods
     -------
-    clean_content(content: str)
-        clean up raw question text to remove examples and useless words
-
     to_word_vector(content: str)
         vectorize the given question content 
     
@@ -24,17 +22,44 @@ class QuestionMatcher(object):
         return all similar questions that exceeds the given threshold value 
         given the question content
     """
+
     def __init__(self, model):
         self.model = model
-    
-    def clean_content(self, content):
-        pass
+
 
     def to_word_vector(self, content):
-        pass
-    
+        content = content.split(' ')
+        content_vector = []
+        for word in content:
+            try:
+                # ignore if the word is not in the embeddings
+                vector = self.model[word]
+                content_vector.append(vector)
+            except KeyError:
+                pass
+        
+        res_vector = np.mean(content_vector, axis = 0)
+        return res_vector
+
+
     def get_similarity(self, q1, q2):
-        pass
-    
-    def get_recommendations(self, question):
-        pass
+        q1_vec = self.to_word_vector(q1)
+        q2_vec = self.to_word_vector(q2)
+        # compute cosine similarity
+        similarity = np.dot(q1_vec, q2_vec) / (np.linalg.norm(q1_vec) * np.linalg.norm(q2_vec))
+        return 0 if np.isnan(np.sum(similarity)) else similarity
+
+    def get_recommendations(self, question, others, threshold = 0):
+        
+        recommendations = []
+        for q in others: # others is a list of tuples (question_name, question_content)
+            similarity = self.get_similarity(question, q)
+            if similarity > threshold:
+                recommendations.append({
+                    'name': q[0],
+                    'content': q[1],
+                    'score': similarity
+                })
+        recommendations.sort(key = lambda k: k['score'], reverse = True)
+        return recommendations
+
